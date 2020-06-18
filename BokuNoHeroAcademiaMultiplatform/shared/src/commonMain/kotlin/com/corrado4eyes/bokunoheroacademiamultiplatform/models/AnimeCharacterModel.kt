fa776b4b068db002625856d5bf8f72975131fc98
corrado4eyes <corrado@splendo.com>
2020-06-18 10:56:40 +0200

    [#1] Renamed method

--- BokuNoHeroAcademiaMultiplatform/shared/src/commonMain/kotlin/com/corrado4eyes/bokunoheroacademiamultiplatform/models/AnimeCharacterModel.kt
+++ BokuNoHeroAcademiaMultiplatform/shared/src/commonMain/kotlin/com/corrado4eyes/bokunoheroacademiamultiplatform/models/AnimeCharacterModel.kt
@@ -4,9 +4,7 @@ import co.touchlab.stately.ensureNeverFrozen
 import com.corrado4eyes.bokunoheroacademiamultiplatform.data.characters.BNHACharacterResponse
 import com.corrado4eyes.bokunoheroacademiamultiplatform.domain.db.DatabaseHelper
 import com.corrado4eyes.bokunoheroacademiamultiplatform.ktor.BokuNoHeroAcademiaApi
-import kotlinx.coroutines.flow.collect
 import org.koin.core.KoinComponent
-import co.touchlab.stately.ensureNeverFrozen
 
 class AnimeCharacterModel(
     private val kTorApi: BokuNoHeroAcademiaApi,
@@ -18,7 +16,7 @@ class AnimeCharacterModel(
     }
 
     private suspend fun storeNewCharactersInLocalDb(characterResponses: List<BNHACharacterResponse>) {
-        dbHelper.insertBreeds(characterResponses)
+        dbHelper.insertCharacter(characterResponses)
     }
 
     private suspend fun fetchCharacters(): List<BNHACharacterResponse> {
