32c819e799357a2c2bd3e2473547ad2c5c940830
corrado4eyes <corrado@splendo.com>
2020-06-18 16:59:41 +0200

    [#5] Move files into Views folder

---
+++ BokuNoHeroAcademiaMultiplatform/ios/BokuNoHeroAcademiaMultiplatform/BokuNoHeroAcademiaMultiplatform/Views/Home/BNHACharacterCell.swift
@@ -0,0 +1,63 @@
+//
+//  BNHACharacterCell.swift
+//  BokuNoHeroAcademiaMultiplatform
+//
+//  Created by Corrado Quattrocchi on 18/06/2020.
+//  Copyright © 2020 Corrado Quattrocchi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import shared
+import Kingfisher
+
+class BNHACharacterCell : UITableViewCell {
+    
+    // MARK: View Properties
+    
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var characterClass: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var quirk: UILabel!
+    
+    // MARK: Business Logic Properties
+    let defaultServiceFactory = DefaultServiceFactory()
+    
+    private var character: BNHACharacterResponse?
+    lazy var imageHelper: Helper = {
+        return self.defaultServiceFactory.getImageHelper()
+    }()
+    
+    // MARK: Public Methods
+    
+    func configure(character: BNHACharacterResponse) {
+        self.character = character
+        self.configureCell()
+
+    }
+    
+    // MARK: Private Methods
+    
+    private func configureCell() {
+        guard let character = self.character else { return }
+        configureImage(url: character.image)
+        name.text = character.name
+        characterClass.text = character.characterClass
+        category.text = character.category
+        quirk.text = character.quirk
+    }
+    
+    private func configureImage(url: String) {
+        guard let url = URL(string: url) else { return }
+//        imageHelper.getImage(url: url) { [weak self] result in
+//            switch result {
+//                case .success(let image):
+//                    self?.characterImage.image = UIImage(data: image)
+//                case .failure(let error):
+//                    print(error)
+//            }
+//        }
+        characterImage.kf.setImage(with: url)
+    }
+}
